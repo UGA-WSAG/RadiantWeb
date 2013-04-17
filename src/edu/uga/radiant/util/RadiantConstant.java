db65ddfcb171968bd5d374c484e261b6494e65d9
Michael E. Cotterell <mepcotterell@gmail.com>
2013-04-16 23:13:28 -0400

    removed old

--- src/edu/uga/radiant/util/RadiantConstant.java
+++
@@ -1,8 +0,0 @@
-package edu.uga.radiant.util;
-
-public class RadiantConstant {
-
-	/** the administrator account type constant - 'admin' */
-	public static final String ACCOUNT_ADMIN = "admin";
-	
-}
